f2ba13205aea25e4b98d616cea9046e61f1193f3
tassin_e <erwan.tassin@epita.fr>
2019-04-13 18:24:05 +0200

    UI done

---
+++ Gacha2019Team3/Assets/score.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class score : MonoBehaviour
+{
+    int scoring;
+    public int bodies = 1;
+    bool done = false;
+
+    void Update()
+    {
+        if (done == false)
+        {
+            done = true;
+            StartCoroutine(wait());
+        }
+    }
+
+    IEnumerator wait()
+    {
+        yield return new WaitForSeconds(1);
+        scoring = (scoring + bodies);
+        this.gameObject.GetComponent<Text>().text = scoring.ToString();
+        done = false;
+        yield return null;
+    }
+}
---
+++ Gacha2019Team3/Assets/timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class timer : MonoBehaviour
+{
+    public float time;
+    int showtime;
+
+    void Update()
+    {
+        time -= Time.deltaTime;
+
+        if (time <= 0)
+        {
+            this.gameObject.GetComponent<Text>().text = "WIN";
+        }
+        else
+        {
+            showtime = (int)time;
+            this.gameObject.GetComponent<Text>().text = showtime.ToString();
+        }
+    }
+}
