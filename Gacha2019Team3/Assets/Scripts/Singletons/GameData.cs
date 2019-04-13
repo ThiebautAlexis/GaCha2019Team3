53fd8b7cbdd54a79b6cfe86e16925396f526640c
Vayne Brice <vayne.brice@gmail.com>
2019-04-13 20:06:05 +0200

    Add Mavie

---
+++ Gacha2019Team3/Assets/Scripts/Singletons/GameData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameData : Singleton<GameData>
+{
+    [Header("Basic Variables")]
+    public TileManager m_TileManager = null;
+    public List<SnakeHead> m_Players;
+
+    public Sprite m_BackgroundSprite = null;
+       
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
