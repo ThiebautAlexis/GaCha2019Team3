24c1636282232079e9859d05a166f10834fb345c
Antoine-PROG <contact.chaumin@gmail.com>
2019-04-14 14:45:06 +0200

    Plein de truk magic

--- Gacha2019Team3/Assets/Scripts/Script_Arnaud/ItemManager.cs
+++ Gacha2019Team3/Assets/Scripts/Script_Arnaud/ItemManager.cs
@@ -6,13 +6,12 @@ using UnityEngine.UI;
 public class ItemManager : Singleton<ItemManager>
 {
     public bool hasItemInStorage;
-    
+
     public GameObject itemPrefab;
     public bool isItemOnMap;
     public bool isProtected;
     //public GameObject lastLinkPos;
     //public GameObject spawnBulletPoint;
-    public Transform m_Entities;
     bool flag;
     public GameObject imageUI;
     public int nbrItemAllowedOnMap;
@@ -21,7 +20,7 @@ public class ItemManager : Singleton<ItemManager>
 
 
 
-    
+
 
     // Start is called before the first frame update
     void Start()
@@ -38,57 +37,70 @@ public class ItemManager : Singleton<ItemManager>
             flag = true;
         }
 
-        imageUI.SetActive(hasItemInStorage);
     }
 
+    public void SetImageUIActive(bool _IsActive)
+    {
+        imageUI.SetActive(_IsActive);
+    }
 
     void SpawnItem()
     {
-	    if(GameData.Instance.m_TileManager.GetEmptyTiles()!=null)
-	    {
-        	List<CustomTile> myEmptyTiles = GameData.Instance.m_TileManager.GetEmptyTiles();
-        	int randNum = Mathf.RoundToInt(Random.Range(0, myEmptyTiles.Count - 1));
-        	Vector2Int position;
-        	position = GameData.Instance.m_TileManager.GetPosition(myEmptyTiles[randNum]);
-        	itemPrefab.transform.position = new Vector3(position.x, -position.y, 0) * 0.5f;
-        	Instantiate(itemPrefab, m_Entities);
-        	GameData.Instance.m_TileManager.m_MapTile[position.x, position.y].m_Entities.Add(itemPrefab);
-        	nbrItemOnMap++;
-        	flag = false;
-	    }
+        if (GameData.Instance.m_TileManager.GetEmptyTiles() != null)
+        {
+            List<CustomTile> myEmptyTiles = GameData.Instance.m_TileManager.GetEmptyTiles();
+            int randNum = Mathf.RoundToInt(Random.Range(0, myEmptyTiles.Count - 1));
+            Vector2Int position;
+            position = GameData.Instance.m_TileManager.GetPosition(myEmptyTiles[randNum]);
+            itemPrefab.transform.position = GameData.Instance.m_TileManager.TilePositionToWorldPosition(position);
+
+            GameObject item = Instantiate<GameObject>(itemPrefab, GameData.Instance.m_EntitiesContainerTransform);
+            GameData.Instance.m_TileManager.m_MapTile[position.x, position.y].m_Entities.Add(item);
+            nbrItemOnMap++;
+            flag = false;
+        }
     }
 
-    public bool CheckItem(Vector2Int position)
+    public bool CheckItem(Vector2Int position, out Item _Item)
     {
-       
+
         if (GameData.Instance.m_TileManager.m_MapTile[position.x, position.y].m_Entities.Count > 0)
         {
             for (int i = 0; i < GameData.Instance.m_TileManager.m_MapTile[position.x, position.y].m_Entities.Count; i++)
             {
-                if (GameData.Instance.m_TileManager.m_MapTile[position.x, position.y].m_Entities[i].GetComponent<Item>() != null)
+                Item item = GameData.Instance.m_TileManager.m_MapTile[position.x, position.y].m_Entities[i].GetComponent<Item>();
+                if (item)
                 {
+                    _Item = item;
                     return true;
                 }
 
-                else return false;
+                else
+                {
+                    _Item = null;
+                    return false;
+                }
             }
         }
 
-        else return false;
+        _Item = null;
         return false;
     }
 
-    public void DestroyItem(Vector2Int position)
+    public void DestroyItem(Vector2Int position, Item _ToDestroy)
     {
-        if (CheckItem(position))
+        if (!_ToDestroy)
         {
-            GameData.Instance.m_TileManager.m_MapTile[position.x, position.y].m_Entities.Remove(GameData.Instance.m_TileManager.m_MapTile[position.x, position.y].m_Entities[0]);
-            Destroy(GameData.Instance.m_TileManager.m_MapTile[position.x, position.y].m_Entities[0]);
+            return;
         }
+
+        GameData.Instance.m_TileManager.m_MapTile[position.x, position.y].m_Entities.Remove(_ToDestroy.gameObject);
+        DestroyImmediate(_ToDestroy.gameObject);
+        --nbrItemOnMap;
     }
 
 
-   
+
 
 
 }
