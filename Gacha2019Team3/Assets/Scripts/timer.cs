5789178e80bdd0fa56cba495a49409f52357f6c4
tassin_e <erwan.tassin@epita.fr>
2019-04-14 14:36:59 +0200

    reversed timer

--- Gacha2019Team3/Assets/Scripts/timer.cs
+++ Gacha2019Team3/Assets/Scripts/timer.cs
@@ -7,29 +7,29 @@ using UnityEngine.SceneManagement;
 public class timer : MonoBehaviour
 {
     public float time;
-    int maxtime;
+    //int maxtime;
     int showtime;
     public string scenename;
 
     private void Start()
     {
-        maxtime = (int)time;
+        //maxtime = (int)time;
     }
 
     void Update()
     {
-        time -= Time.deltaTime;
+        time += Time.deltaTime;
 
-        if (time <= 0)
+        /*if (time <= 0)
         {
             EndData.Instance.win = true;
             SceneManager.LoadScene(scenename);
         }
         else
-        {
+        {*/
             showtime = (int)time;
-            EndData.Instance.UpdateTime(maxtime - showtime);
+            EndData.Instance.UpdateTime(/*maxtime - */showtime);
             this.gameObject.GetComponent<Text>().text = showtime.ToString();
-        }
+        //}
     }
 }
